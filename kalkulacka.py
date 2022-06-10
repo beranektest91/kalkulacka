6fd8e32eea1707aecf29105450aef7e98c30cdf7
beranektest91 <107270065+beranektest91@users.noreply.github.com>
2022-06-10 18:50:37 +0200

    First commit

---
+++ kalkulacka.py
@@ -0,0 +1,15 @@
+class Kalkulacka:
+
+    @classmethod
+    def secti(cls, a, b):
+        return a + b
+
+    @classmethod
+    def odecti(cls, a, b):
+        return a - b
+
+def main():
+    print(Kalkulacka.secti(5, 6))
+
+if __name__ == "__main__":
+    main()
